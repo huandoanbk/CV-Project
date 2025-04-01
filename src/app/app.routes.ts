b9d1bb3b3cf87a483f45573d4968ed932e034d6d
Huan <huandoan89@gmail.com>
2025-04-02 01:29:06 +0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/about',pathMatch: 'full' },
+    {path:'about',component: AboutMeComponent},
+    {path:'skills', component: SkillsComponent},
+    {path: 'experience',component: ExperienceComponent}
+];
